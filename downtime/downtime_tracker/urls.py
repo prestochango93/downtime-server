8bae5ca5fe0e1258e74d72d911a67ff12ef664e7
prestochango93 <prestochango93@gmail.com>
2026-02-09 14:51:10 -0500

    Add URL patterns for downtime tracker views

---
+++ downtime/downtime_tracker/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "downtime_tracker"
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("dept/<slug:code>/", views.department_detail, name="department_detail"),
+    path("equipment/<int:pk>/status/", views.change_status, name="change_status"),
+]
