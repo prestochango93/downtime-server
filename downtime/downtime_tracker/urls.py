746ae596e526f2ff2cad6ac4c36665867afe5e0a
prestochango93 <prestochango93@gmail.com>
2026-02-10 07:41:08 -0500

    Add equipment detail URL pattern

--- downtime/downtime_tracker/urls.py
+++ downtime/downtime_tracker/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("dept/<slug:code>/", views.department_detail, name="department_detail"),
     path("equipment/<int:pk>/status/", views.change_status, name="change_status"),
+    path("equipment/<int:pk>/", views.equipment_detail, name="equipment_detail"),
 ]
