9f9d5e8c97d3738af1bcab24bf9e1c864a99291b
prestochango93 <prestochango93@gmail.com>
2026-02-10 07:35:44 -0500

    Implement get_item filter in dt_extras.py
    Add a custom template filter to retrieve items from a mapping.

--- downtime/downtime_tracker/templatetags/dt_extras.py
+++ downtime/downtime_tracker/templatetags/dt_extras.py
@@ -1,1 +1,9 @@
-dsafd
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_item(mapping, key):
+    if mapping is None:
+        return None
+    return mapping.get(key)
