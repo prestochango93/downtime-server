d7854d5b8455ddc892a38ab10e7dfa79a710d360
prestochango93 <prestochango93@gmail.com>
2026-02-13 10:17:17 -0500

    Update settings.py

--- downtime/downtime/settings.py
+++ downtime/downtime/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#u!t0k2q=3%f1c+v4^g!8@p2x*9=7n$1z!q4y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'FALSE'
+DEBUG = False
 
 ALLOWED_HOSTS = ["192.168.1.17","localhost","127.0.0.1"]
 
