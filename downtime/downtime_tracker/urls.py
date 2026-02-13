2b7c509078d57b5bb28db56f986836e0f96b9c42
prestochango93 <prestochango93@gmail.com>
2026-02-13 09:09:45 -0500

    Update urls.py

--- downtime/downtime_tracker/urls.py
+++ downtime/downtime_tracker/urls.py
@@ -5,9 +5,14 @@ app_name = "downtime_tracker"
 
 urlpatterns = [
     path("", views.home, name="home"),
+
+    # NEW: Plant Dashboard
+    path("dashboard/", views.plant_dashboard, name="plant_dashboard"),
+
     path("dept/<slug:code>/", views.department_detail, name="department_detail"),
-    path("equipment/<int:pk>/status/", views.change_status, name="change_status"),
-    path("equipment/<int:pk>/", views.equipment_detail, name="equipment_detail"),
     path("dept/<slug:code>/export.csv", views.department_export_csv, name="department_export_csv"),
+
+    path("equipment/<int:pk>/", views.equipment_detail, name="equipment_detail"),
     path("equipment/<int:pk>/export.csv", views.equipment_export_csv, name="equipment_export_csv"),
+    path("equipment/<int:pk>/status/", views.change_status, name="change_status"),
 ]
